6ac18170099f314d9f0c9459598725e9d330fd37
DomenicoAgostino <agostino.domenico96@gmail.com>
2022-04-20 17:33:56 +0200

    Prompt per inserire parola

---
+++ Palidroma/script.js
@@ -0,0 +1,10 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+
+// chiedo la parola
+const userWord = prompt("Inserisci una parola");
+
+
