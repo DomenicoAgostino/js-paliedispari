9ae7b3bb7f960d4077eca28952c3dba6c60274eb
DomenicoAgostino <agostino.domenico96@gmail.com>
2022-04-21 08:58:42 +0200

    Esericizio Palindroma terminato

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -7,4 +7,19 @@ Creare una funzione per capire se la parola inserita è palindroma
 // chiedo la parola
 const userWord = prompt("Inserisci una parola");
 
+function isPalindrome(string) {
+ 
+  const wordLength = string.length - 1;
 
+  for (let i = 0; i < wordLength / 2; i++) {
+
+    if (string[i] !== string[wordLength - i] ) {
+      return document.getElementById("text-pali").innerHTML = "Questa parola non e' palindroma";
+    }
+
+    return document.getElementById("text-pali").innerHTML = "Questa parola  e' palindroma";
+  } 
+
+}
+// Chiamo Funzione
+isPalindrome(userWord);
